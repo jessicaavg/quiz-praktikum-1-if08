da8c635dff3a889a86e503b8e428f80a74ed0bf3
fuguhverdian <fahrulfuguhv@students.amikom.ac.id>
2023-04-13 14:30:28 +0700

    perubahan mode

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,31 @@
 function insrt(num) {
-	$('.tampil').val($('.tampil').val() + num);
+  $('.tampil').val($('.tampil').val() + num);
 }
 function eql() {
-	$('.tampil').val(eval($('.tampil').val()));
+  $('.tampil').val(eval($('.tampil').val()));
 }
 function AC() {
-	$('.tampil').val('');
+  $('.tampil').val('');
 }
 function del() {
-	value = $('.tampil').val();
-	$('.tampil').val(value.substring(0, value.length - 1));
+  value = $('.tampil').val();
+  $('.tampil').val(value.substring(0, value.length - 1));
 }
-  
+
+$('#lightmode-togle').change(() => {
+  if ($('#lightmode-togle').is(':checked')) {
+    $('.main').css('background-color', '#dbd7d7');
+    $('.tampil').css('color', '#39383d');
+    $('.btninput').css('background-color', '#f1ebdf');
+    $('.tombol').css('color', 'black');
+    $('#ijo').css('color', ' #13d1b8');
+    $('#merah').css('color', ' #d84e55');
+  } else {
+    $('.main').css({ 'background-color': '#292d36' });
+    $('.tampil').css('color', '#f1ebdf');
+    $('.btninput').css('background-color', '#39383d');
+    $('.tombol').css('color', '#f1ebdf');
+	$('#ijo').css('color', ' #13d1b8');
+    $('#merah').css('color', ' #d84e55');
+  }
+});
